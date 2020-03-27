98d897f2d62b1e8fef0d75d2b3862876879d8cec
Dieter Lunn <coder2000@gmail.com>
2020-03-27 16:31:08 -0500

    Initial commit

---
+++ Ubiety.Build.Tool/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ubiety.Build.Tool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
